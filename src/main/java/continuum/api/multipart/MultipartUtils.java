a567cd2738dcc7b445205fc69b361bdbe00077d0
Madsthunder <madsthunder109@gmail.com>
2016-09-25 23:37:40 -0400

    Added some methods that could be useful

--- src/main/java/continuum/api/multipart/MultipartUtils.java
+++ src/main/java/continuum/api/multipart/MultipartUtils.java
@@ -1,14 +1,24 @@
 package continuum.api.multipart;
 
+import javax.annotation.Nullable;
+
+import com.google.common.collect.BiMap;
+
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.registry.IForgeRegistry;
 
 public class MultipartUtils
 {
 
+	public static final ResourceLocation BLOCK_TO_MULTIPART = new ResourceLocation("ctmultipart", "blocktomultipart");
+	public static final ResourceLocation ITEM_TO_MULTIPART = new ResourceLocation("ctmultipart", "itemtomultipart");
+	
 	public static MultipartState getSelectedInfo(Block testFor, EntityPlayer player, IBlockAccess access)
 	{
 		RayTraceResult result = ForgeHooks.rayTraceEyes(player, player.capabilities.isCreativeMode ? 4.5D : 5D);
@@ -18,4 +28,29 @@ public class MultipartUtils
 			return null;
 	}
 	
+	public static BiMap<Block, Multipart> getBlockToMultipartMap()
+	{
+		IForgeRegistry<Multipart> multipartRegistry = getMultipartRegistry();
+		if(multipartRegistry != null)
+		{
+			return multipartRegistry.getSlaveMap(BLOCK_TO_MULTIPART, BiMap.class);
+		}
+		return null;
+	}
+	
+	public static BiMap<Block, Multipart> getItemToMultipartMap()
+	{
+		IForgeRegistry<Multipart> multipartRegistry = getMultipartRegistry();
+		if(multipartRegistry != null)
+		{
+			return multipartRegistry.getSlaveMap(ITEM_TO_MULTIPART, BiMap.class);
+		}
+		return null;
+	}
+	
+	@Nullable
+	public static IForgeRegistry<Multipart> getMultipartRegistry()
+	{
+		return GameRegistry.findRegistry(Multipart.class);
+	}
 }
